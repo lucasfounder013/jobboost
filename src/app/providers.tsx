461c2c421a3a0437b269b640fab042da72802c40
Lucas Ledonne <lucasledonne@live.fr>
2026-06-04 23:38:07 +0200

    fix: PostHog authorized URL jobboost.fr

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -10,6 +10,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
       api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
       person_profiles: "identified_only",
       capture_pageview: false,
+      capture_pageleave: true,
     });
   }, []);
 
